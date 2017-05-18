7ea1e99f2843bce19ee1b7010c049b03414a03fb
Mehdi Druon <mehdidruon@gmail.com>
2017-05-18 12:30:08 +0200

    modif arborescence

---
+++ Frontend/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Landing from './landing/landing.js';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Landing></Landing>
+      </div>
+    );
+  }
+}
+
+export default App;
