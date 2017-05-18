068aa8e0cbea5e0182c2459815151cb2df2042ec
Hilal Bouigoumane <hilal.bouigoumane@gmail.com>
2017-05-18 12:01:07 +0200

    add files

---
+++ src/landing.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Background from '../public/polygon-1980897_1920.jpg';
+
+
+class Landing extends Component {
+  render() {
+    return (
+      <div className="Landing">
+          <img src="{Background}"/>
+      </div>
+    );
+  }
+}
+
+export default Landing;
